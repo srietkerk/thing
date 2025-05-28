b248156773aaaf9122bf9c5934f14df9a082a9ab
Sarah Rietkerk <49178322+srietkerk@users.noreply.github.com>
2025-05-28 14:27:02 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let ost = "hello govma"
+let sst = ost.substr(4, 0)
+let myImage = images.createImage(`
+    # . . . .
+    . # # # .
+    . . # # .
+    . . . # .
+    . . . . #
+    `)
+myImage.showImage(0)
